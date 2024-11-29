c44e9d2fbfd24dc65bde6f10548681041fc5b863
Manthan Patel <0xmanthanpatel@gmail.com>
2024-11-29 23:39:07 +0530

    d

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,6 @@ import { useRouter } from "next/navigation";
 
 export default function page() {
   const router = useRouter();
-  router.push("bid/faculty");
+  router.push("bid/legendry");
   return <div></div>;
 }
